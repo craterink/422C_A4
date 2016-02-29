740bb55988f63195bc84f98c30ffd6bcb8bcf3f7
Cooper Raterink <craterink@utexas.edu>
2016-02-28 20:40:47 -0600

    Added junit testcases file

---
+++ assignment4/WordLadderTests.java
@@ -0,0 +1,25 @@
+package assignment4;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WordLadderTests {
+
+	@Before
+	public void setUp() throws Exception {
+	
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
